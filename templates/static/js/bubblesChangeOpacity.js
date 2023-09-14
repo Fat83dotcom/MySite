0e785e57c66ec1e4a67ebd6904df9c29de468419
fat83dotcom <fernando.quimic@gmail.com>
2023-09-13 21:04:50 -0300

    Novos elementos do site.

---
+++ templates/static/js/bubblesChangeOpacity.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', ()=>{
+    let element = document.getElementById('bubbles')
+    element.style.display = 'flex'
+    setTimeout(()=>{
+        element.style.opacity = '1'
+    }, 3000)
+})

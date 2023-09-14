53dd0a093a27fd51bad224ddf82e8857aee510cc
fat83dotcom <fernando.quimic@gmail.com>
2023-09-13 21:04:10 -0300

    ClassView para cada seção do site.

--- Core/urls.py
+++ Core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from Core.views import IndexView
+from Core.views import IndexView, BlogView, PortfolioView, AboutView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('blog/', BlogView.as_view(), name='blog'),
+    path('portfolio/', PortfolioView.as_view(), name='portfolio'),
+    path('about/', AboutView.as_view(), name='about'),
 ]
--- Core/views.py
+++ Core/views.py
@@ -5,3 +5,18 @@ from django.views import View
 class IndexView(View):
     def get(self, request):
         return render(request, 'site/index.html')
+
+
+class BlogView(View):
+    def get(self, request):
+        return render(request, 'blog/blog.html')
+
+
+class PortfolioView(View):
+    def get(self, request):
+        return render(request, 'portfolio/portfolio.html')
+
+
+class AboutView(View):
+    def get(self, request):
+        return render(request, 'about/about.html')

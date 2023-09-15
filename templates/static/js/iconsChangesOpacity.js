caac499bc8a7fae7fae8a4b4d5a9a78a929891c0
fat83dotcom <fernando.quimic@gmail.com>
2023-09-15 00:08:24 -0300

    JS para revelar os icones.

---
+++ templates/static/js/iconsChangesOpacity.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', ()=>{
+    let element = document.getElementById('icons')
+    element.style.display = 'flex'
+    setTimeout(()=>{
+        element.style.opacity = '1'
+    }, 2000)
+})

85047b5eb5a5c1a9a893f34e059164ef0a9ca8f2
fat83dotcom <fernando.quimic@gmail.com>
2023-09-16 19:46:19 -0300

    Urls do blog.

--- Core/urls.py
+++ Core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from Core.views import IndexView, BlogView, PortfolioView, AboutView
+from Core.views import IndexView, PortfolioView, AboutView
+from Core.views import BlogPostView, BlogView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('blog/', BlogView.as_view(), name='blog'),
     path('portfolio/', PortfolioView.as_view(), name='portfolio'),
     path('about/', AboutView.as_view(), name='about'),
+    path('blog/', BlogView.as_view(), name='blogIndex'),
+    path('blog/post', BlogPostView.as_view(), name='blogPost'),
 ]

2e2a2d3e3a082c153f83dee3029f9d153e420c60
fat83dotcom <fernando.quimic@gmail.com>
2023-09-17 10:49:32 -0300

    Registro dos models na area administrativa.

--- Core/admin.py
+++ Core/admin.py
@@ -1,1 +1,26 @@
 from django.contrib import admin
+from Core.models import Tag, Category
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug'
+    list_display_links = 'name', 'slug'
+    list_filter = 'name', 'slug'
+    list_per_page = 10
+    ordering = '-id',
+    # prepopulated_fields = {
+    #     'slug': ('name',),
+    # }
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug'
+    list_display_links = 'name', 'slug'
+    list_filter = 'name', 'slug'
+    list_per_page = 10
+    ordering = '-id',
+    # prepopulated_fields = {
+    #     'slug': ('name',),
+    # }

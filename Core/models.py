8fedc7ab3c4b1b4a0e0197f05be4ad4d4b6e7968
fat83dotcom <fernando.quimic@gmail.com>
2023-09-17 10:45:40 -0300

    Implementação das models Tag e Category

--- Core/models.py
+++ Core/models.py
@@ -1,1 +1,48 @@
 from django.db import models
+from utils.randstr import slugfyNew
+
+
+class Tag(models.Model):
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True,
+        default=None,
+        null=True,
+        blank=True,
+        max_length=255,
+    )
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.slug:
+            self.slug = slugfyNew(self.name, 10)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True,
+        default=None,
+        null=True,
+        blank=True,
+        max_length=255,
+    )
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.slug:
+            self.slug = slugfyNew(self.name, 10)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name

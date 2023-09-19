8d77134ec14373e325bef533f279f06d1f3f3f3d
fat83dotcom <fernando.quimic@gmail.com>
2023-09-19 18:48:30 -0300

    Post admin e implementação do summernote.

--- Core/admin.py
+++ Core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from Core.models import Tag, Category, Page
+from Core.models import Tag, Category, Page, Post
+from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Tag)
@@ -28,11 +29,37 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
-    list_display = 'id', 'title', 'slug'
-    list_display_links = 'title', 'slug'
+    list_display = 'id', 'title', 'slug', 'isPublished', 'content'
+    search_fields = 'id', 'title', 'slug', 'isPublished'
+    list_display_links = 'title', 'slug', 'isPublished'
     list_filter = 'title', 'slug'
     list_per_page = 10
     ordering = '-id',
     # prepopulated_fields = {
     #     'slug': ('name',),
     # }
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = 'id', 'title', 'isPublished', 'createdBy',
+    list_display_links = 'title',
+    search_fields = 'id', 'slug', 'title', 'excerpt',
+    list_per_page = 10
+    list_filter = 'isPublished',
+    list_editable = 'isPublished',
+    ordering = '-id',
+    readonly_fields = 'createdAt', 'updatedAt', 'updatedBy', 'createdBy',
+    prepopulated_fields = {
+        'slug': ('title',),
+    }
+    summernote_fields = ('content',)
+    # autocomplete_fields = 'tagKey', 'categoryKey',
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.updatedBy = request.user
+        else:
+            obj.createdBy = request.user
+
+        obj.save()

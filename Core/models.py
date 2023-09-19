51537fe7a1add950ffe91aaf64bbe2fde2f68b2c
fat83dotcom <fernando.quimic@gmail.com>
2023-09-19 19:19:09 -0300

    Crição da classe post e manager post.

--- Core/models.py
+++ Core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from utils.randstr import slugfyNew
+from django.contrib.auth.models import User
+from utils.images import resizeImage
 
 
 class Tag(models.Model):
@@ -49,6 +51,10 @@ class Category(models.Model):
 
 
 class Page(models.Model):
+    class Meta:
+        verbose_name = 'Page'
+        verbose_name_plural = 'Pages'
+
     title = models.CharField(max_length=255)
     slug = models.SlugField(
         unique=True,
@@ -69,3 +75,69 @@ class Page(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class PostManager(models.Manager):
+    def getIsPublished(self):
+        return self.filter(isPublished=True).order_by('-id')
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    objects = PostManager()
+
+    title = models.CharField(max_length=255)
+    excerpt = models.CharField(max_length=255)
+    content = models.TextField()
+    cover = models.ImageField(
+        upload_to='posts/%Y/%m/', blank=True, default=''
+    )
+    coverInPostContent = models.BooleanField(
+        default=True,
+        help_text='Exibe a imagem no conteudo do post.'
+    )
+    createdAt = models.DateTimeField(auto_now_add=True)
+    createdBy = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='postCreatedBy'
+    )
+    updatedAt = models.DateTimeField(auto_now=True)
+    updatedBy = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='postUpdatedBy'
+    )
+    categoryKey = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        null=True, blank=True,
+        default=None
+    )
+    tagKey = models.ManyToManyField(Tag, blank=True, default='')
+    slug = models.SlugField(
+        unique=True,
+        default='',
+        null=False,
+        blank=True,
+        max_length=255
+    )
+    isPublished = models.BooleanField(
+        default=False, help_text='Marque para publicar.'
+    )
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.slug:
+            self.slug = slugfyNew(self.title, 10)
+
+        super().save(*args, **kwargs)
+
+        if self.cover:
+            resizeImage(self.cover, 900)

7a01e062ab8375aadc97e7790eaf84a504f57d09
fat83dotcom <fernando.quimic@gmail.com>
2023-09-19 23:41:28 -0300

    Novas urls para novas funcionalidades.

--- Core/urls.py
+++ Core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from Core.views import IndexView, PortfolioView, AboutView
-from Core.views import BlogPostView, BlogView
+from Core.views import BlogPostView, BlogView, CategoryView
+from Core.views import TagView, SearchView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
@@ -8,4 +9,7 @@ urlpatterns = [
     path('about/', AboutView.as_view(), name='about'),
     path('blog/', BlogView.as_view(), name='blogIndex'),
     path('blog/post/<slug:slug>/', BlogPostView.as_view(), name='blogPost'),
+    path('category/<slug:slug>/', CategoryView.as_view(), name='category'),
+    path('tag/<slug:slug>/', TagView.as_view(), name='tag'),
+    path('search/', SearchView.as_view(), name='search'),
 ]

fcb879e7d8fddbe17c86c53b843470063a5d97cd
fat83dotcom <fernando.quimic@gmail.com>
2023-09-19 23:42:34 -0300

    Novas views para as consultas, tags e categories.

--- Core/views.py
+++ Core/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.views import View
 from Core.models import Post
+from django.db.models import Q
+
+
+PER_PAGE = 9
 
 
 class IndexView(View):
@@ -12,7 +16,7 @@ class IndexView(View):
 class BlogView(View):
     def get(self, request):
         result = Post.objects.getIsPublished()
-        paginator = Paginator(result, 9)
+        paginator = Paginator(result, PER_PAGE)
         pageNumber = request.GET.get('page')
         pageObj = paginator.get_page(pageNumber)
         context = {
@@ -28,6 +32,48 @@ class BlogPostView(View):
             'post': result,
         }
         return render(request, 'blog/post.html', context)
+
+
+class CategoryView(View):
+    def get(self, request, slug):
+        result = Post.objects.getIsPublished().filter(
+            categoryKey__slug=slug
+        )
+        paginator = Paginator(result, PER_PAGE)
+        pageNumber = request.GET.get('page')
+        pageObj = paginator.get_page(pageNumber)
+        context = {
+            'posts': pageObj,
+        }
+        return render(request, 'blog/blogIndex.html', context)
+
+
+class TagView(View):
+    def get(self, request, slug):
+        result = Post.objects.getIsPublished().filter(
+            tagKey__slug=slug
+        )
+        context = {
+            'posts': result,
+        }
+        return render(request, 'blog/blogIndex.html', context)
+
+
+class SearchView(View):
+    def get(self, request):
+        search: str = request.GET.get('search').strip()
+        result = Post.objects.getIsPublished().filter(
+            Q(title__icontains=search) |
+            Q(excerpt__icontains=search) |
+            Q(content__icontains=search)
+        )
+        paginator = Paginator(result, PER_PAGE)
+        pageNumber = request.GET.get('page')
+        pageObj = paginator.get_page(pageNumber)
+        context = {
+            'posts': pageObj,
+        }
+        return render(request, 'blog/blogIndex.html', context)
 
 
 class PortfolioView(View):
